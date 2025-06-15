4f5a9b5c40b3c1d2a6aabc14438aa4eef7aeb5e5
rimshaa2 <153751846+rimshaa2@users.noreply.github.com>
2025-06-15 14:14:19 +0500

    Update test_add_task.py

--- tests/test_add_task.py
+++ tests/test_add_task.py
@@ -9,7 +9,7 @@ def test_add_task():
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("http://51.20.42.102:8081/app/index.php")
+    driver.get("http://13.51.166.46:8081")
 
     # Replace the following with actual logic
     task_input = driver.find_element("name", "task")
